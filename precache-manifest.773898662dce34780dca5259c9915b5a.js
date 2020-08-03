7690cb4e36cd22dbeeb99b7c2c1e01b97cb060e2
amman <aman.kapoor@1mg.com>
2020-08-04 01:53:56 +0530

    Updates

---
+++ precache-manifest.773898662dce34780dca5259c9915b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73616a4491c17c7fcd951ad0480d9c7",
+    "url": "/spinny-assignment/index.html"
+  },
+  {
+    "revision": "5fb81cfd511610124350",
+    "url": "/spinny-assignment/static/css/main.d9214cea.chunk.css"
+  },
+  {
+    "revision": "c9b7db370f51a2092025",
+    "url": "/spinny-assignment/static/js/2.bf648f25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spinny-assignment/static/js/2.bf648f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb81cfd511610124350",
+    "url": "/spinny-assignment/static/js/main.293dead6.chunk.js"
+  },
+  {
+    "revision": "ab465b046a7a45621dd3",
+    "url": "/spinny-assignment/static/js/runtime-main.ab2d892f.js"
+  }
+]);
